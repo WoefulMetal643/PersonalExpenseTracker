91f03bcc8c2ca7ff5966b91e539204fbcc962936
Victor Duenas <138256039+WoefulMetal643@users.noreply.github.com>
2024-11-29 18:11:55 -0700

    Added how it will look

--- Personal Expense Tracker/src/PersonalExpenseTracker.java
+++ Personal Expense Tracker/src/PersonalExpenseTracker.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class PersonalExpenseTracker{
 	
@@ -57,16 +58,133 @@ public class PersonalExpenseTracker{
 		JScrollPane scroll = new JScrollPane(list);
 		frame.add(scroll);
 		
+		JButton newExpense = new JButton("new Expense");
+		JButton addCategories = new JButton("add category");
+		JButton view = new JButton("view");
+		JPanel buttonsLayout = new JPanel();
+		buttonsLayout.add(newExpense);
+		buttonsLayout.add(addCategories);
+		buttonsLayout.add(view);
+		frame.add(buttonsLayout, BorderLayout.SOUTH);
+		
+		newExpense.addActionListener(e-> {
+			newExpense();
+			frame.setVisible(false);
+		});
+		
+		addCategories.addActionListener(e-> {
+			newCategory();
+			frame.setVisible(false);
+		});
+		
+		view.addActionListener(e-> {
+			view();
+			frame.setVisible(false);
+		});
+		
 		frame.setVisible(true);
 	}
 	
 	public static void newExpense(){
+		PersonalExpenseTracker tracker = new PersonalExpenseTracker();
 		JFrame frame = new JFrame("NewExpenseTracker");
 		frame.setVisible(true);
 		frame.setSize(300, 200);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel nameExpense = new JLabel("Enter new Expense:");
+		nameExpense.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		panel.add(nameExpense);
+		
+		JTextField text = new JTextField();
+		text.setMaximumSize(new java.awt.Dimension(200, 30));
+		panel.add(text);
+		
+		frame.add(panel);
+		
+		JButton addExpense = new JButton("add");
+		JButton back = new JButton("Back");
+		JPanel buttonsLayout = new JPanel();
+		buttonsLayout.add(addExpense);
+		buttonsLayout.add(back);
+		frame.add(buttonsLayout, BorderLayout.SOUTH);
+		
+		addExpense.addActionListener(e-> {
+			tracker.mainMenu();
+			frame.setVisible(false);
+		});
+		
+		back.addActionListener(e-> {
+			tracker.mainMenu();
+			frame.setVisible(false);
+		});
+		frame.setVisible(true);
+	}
+	
+	public static void newCategory(){
+		PersonalExpenseTracker tracker = new PersonalExpenseTracker();
+		JFrame frame = new JFrame("NewCategory");
+		frame.setVisible(true);
+		frame.setSize(300, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JLabel nameExpense = new JLabel("Enter new Category:");
+		nameExpense.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+		panel.add(nameExpense);
+		
+		JTextField text = new JTextField();
+		text.setMaximumSize(new java.awt.Dimension(200, 30));
+		panel.add(text);
+		
+		frame.add(panel);
+		
+		JButton addExpense = new JButton("add");
+		JButton back = new JButton("Back");
+		JPanel buttonsLayout = new JPanel();
+		buttonsLayout.add(addExpense);
+		buttonsLayout.add(back);
+		frame.add(buttonsLayout, BorderLayout.SOUTH);
+		
+		addExpense.addActionListener(e-> {
+			tracker.mainMenu();
+			frame.setVisible(false);
+		});
+		
+		back.addActionListener(e-> {
+			tracker.mainMenu();
+			frame.setVisible(false);
+		});
+		frame.setVisible(true);
+	}
+	
+	public static void view(){
+		PersonalExpenseTracker tracker = new PersonalExpenseTracker();
+		JFrame frame = new JFrame("ViewExpense");
+		frame.setVisible(true);
+		frame.setSize(300, 200);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton back = new JButton("Back");
+		frame.add(back, BorderLayout.SOUTH);
+		
+		back.addActionListener(e-> {
+			tracker.mainMenu();
+			frame.setVisible(false);
+		});
+		
+		frame.setVisible(true);
 	}
 
 }
